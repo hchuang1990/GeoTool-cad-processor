9ef8f022adcb25d48e7baf0a869c04d7f98d744b
zack.huang <hchuang1990@gmail.com>
2022-10-22 21:17:14 +0800

    dwg2dxf

---
+++ dwg2dxf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from datetime import datetime
+import win32com.client
+
+acad = win32com.client.Dispatch("AutoCAD.Application")
+
+files = ['58年地形套疊圖', '77年地形套疊圖', '83年地形套疊圖']
+
+
+def convert(folder):
+    t = folder.split("\\")
+    project_name = t[len(t) - 1]
+    print(f'project_name={project_name}')
+    try:
+        start_time = datetime.now()
+        print(f'start_time={start_time}')
+        for file in files:
+            acad.ActiveDocument.Application.Documents.Open(f"{folder}\\{project_name}_{file}.dwg")
+
+            # for entity in acad.ActiveDocument.ModelSpace:
+            #     name = entity.EntityName
+            #     if name == 'AcDbBlockReference':
+            #         entity.Explode()
+            #
+            # for entity in acad.ActiveDocument.ModelSpace:
+            #     name = entity.EntityName
+            #     if name == 'AcDbText':
+            #         if '房屋樓層註記' in entity.Layer:
+            #             print(
+            #                 f'text: {entity.TextString} at {entity.InsertionPoint}, layer={entity.Layer}, Alignment = {entity.Alignment}')
+            #             print('update for building')
+            #             entity.Alignment = 0
+            #         if 'LDASHAP' in entity.Layer:
+            #             print(
+            #                 f'text: {entity.TextString} at {entity.InsertionPoint}, layer={entity.Layer}, Alignment = {entity.Alignment}')
+            #             print('update for lands')
+            #             entity.Alignment = 0
+
+            acad.ActiveDocument.SaveAs(f"{folder}\\{project_name}_{file}_dxf2000", 13)
+
+        end_time = datetime.now()
+        print(f'end_time={end_time}')
+        print(f"time consuming={end_time - start_time}")
+        return True, ""
+    except Exception as e:
+        return False, e
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import easygui
 import converter
+import dwg2dxf
 
 inputFolder = easygui.diropenbox(msg='選擇欲轉檔之資料夾')
 print(inputFolder)
 if inputFolder is None:
     print('skip execution')
 else:
-    result, msg = converter.convert(inputFolder)
+    result, msg = dwg2dxf.convert(inputFolder)
     print(f"result={result}, msg={msg}")
