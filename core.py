67f517062969c1350f4cdae044bf986c360d4e9d
zack.huang <hchuang1990@gmail.com>
2022-10-23 22:31:49 +0800

    rename package and format the output

---
+++ core.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import win32com.client
+import pythoncom
+import os
+
+
+def APoint(x, y):
+    """坐标点转化为浮点数"""
+    # 需要两个点的坐标
+    return win32com.client.VARIANT(pythoncom.VT_ARRAY | pythoncom.VT_R8, (x, y))
+
+
+def saveAsDxf(acad, doc, layout, path, file_name, config):
+    try:
+        output = get_dir(path, file_name, config)
+        try:
+            os.mkdir(output[0])
+        except:
+            pass
+        print("print file in Dir = ", output[1])
+        doc.SaveAs(output[1], 13)
+        success = True
+        message = 'Completed'
+    except Exception as e:
+        print(e)
+        success = False
+        message = str(e)
+
+    return success, message
+
+
+def exportFile(acad, doc, layout, path, file_name, config):
+    try:
+        # doc.Application.ZoomExtents()
+        doc.SetVariable("BACKGROUNDPLOT", 0)
+        doc.Plot.QuietErrorMode = True
+
+        Scale = 1
+        # set windows
+        lowerLeft = None
+        underRight = None
+        for entity in acad.ActiveDocument.ModelSpace:
+            if entity.EntityName == 'AcDbPolyline' and entity.Area > 15500 and entity.Layer == "圖框":
+                # print("Layer", entity.Layer)
+                # print("Area", entity.Area)
+                lowerLeft, underRight = entity.GetBoundingBox()
+
+        print("lowerLeft", [lowerLeft[0], lowerLeft[1]], "underRight", [underRight[0], underRight[1]])
+
+        # 打印機
+        layout.ConfigName = config["printer"]
+        # layout.CanonicalMediaName = 'ISO_full_bleed_A2_(594.00_x_420.00_MM)'
+        layout.CanonicalMediaName = config["papper"]  # 图纸大小这里选择A4
+        # layout.PaperUnits = 1  # 图纸单位，1为毫米
+        layout.PlotRotation = 0  # 横向打印
+        layout.StandardScale = 0  # 图纸打印比例
+        layout.CenterPlot = True  # 居中打印
+        layout.PlotWithPlotStyles = True  # 依照样式打印
+        layout.PlotHidden = False  # 隐藏图纸空间对象
+        layout.CenterPlot = True
+        layout.UseStandardScale = True  # 选用标准的比例
+
+        po1 = APoint(lowerLeft[0] * Scale - 1, lowerLeft[1] * Scale)
+        po2 = APoint(underRight[0] * Scale - 1 + 11880, underRight[1] * Scale + 8400)  # 左下点和右上点
+        layout.SetWindowToPlot(po1, po2)
+        # layout.PlotType = 3.5
+        output = get_dir(path, file_name, config)
+        try:
+            os.mkdir(output[0])
+        except:
+            pass
+        print("print file in Dir = ", output[1])
+        doc.Plot.PlotToFile(output[1])
+        success = True
+        message = 'Completed'
+
+    except Exception as e:
+        print(e)
+        success = False
+        message = str(e)
+
+    return success, message
+
+
+def get_dir(path, file_name, config):
+    output_path = os.path.join(path, "output_test", config['format'])
+    output_path_file = f"{output_path}\\{file_name.split('.')[0]}.{config['format']}"
+    return [output_path, output_path_file]
--- handler.py
+++ handler.py
@@ -8,7 +8,7 @@
 # from qtapp import Ui_Dialog
 from os.path import join
 import win32com.client
-import printer
+import core
 import time
 
 
@@ -33,10 +33,12 @@ def handle(config):
                     if config["format"] == 'dwg':
                         pass
                     elif config["format"] == 'dxf':
+                        success, message = core.saveAsDxf(acad=acad, layout=layout, doc=doc, path=path, file_name=dwg,
+                                                          config=config)
                         pass
                     elif config["format"] == 'tiff' or config["format"] == 'pdf' or config["format"] == 'jpg':
-                        success, message = printer.exportFile(acad=acad, layout=layout, doc=doc, path=path, file_name=dwg,
-                                                              config=config)
+                        success, message = core.exportFile(acad=acad, layout=layout, doc=doc, path=path, file_name=dwg,
+                                                           config=config)
                         pass
             except Exception as e:
                 print(e)
@@ -53,6 +55,6 @@ def handle(config):
 dummy_data = {'source_path': 'E:/chiao_study/projects/cad2shp', 'folders': ['A020027'],
               'dwgs': ['A020027_58年地形套疊圖.DWG', 'A020027_77年地形套疊圖.DWG', 'A020027_77年正射影像套疊圖.DWG', 'A020027_83年地形套疊圖.DWG',
                        'A020027_83年正射影像套疊圖.DWG', 'A020027_相關位置圖(107年地形圖).DWG'], 'explode': False, 'saveAs': True,
-              'format': 'pdf', 'printer': 'DWG To PDF.pc3', 'papper': 'ISO_full_bleed_A2_(594.00_x_420.00_MM)',
+              'format': 'dxf', 'printer': 'DWG To PDF.pc3', 'papper': 'ISO_full_bleed_A2_(594.00_x_420.00_MM)',
               'log_path': 'E:chiao_studyprojectscad2shptestrun...'}
 handle(dummy_data)
